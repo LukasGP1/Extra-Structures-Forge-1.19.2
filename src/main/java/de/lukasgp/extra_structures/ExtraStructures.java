fe0e91a0783c78315a657117c2f8e6f32801781c
LukasGP1 <lukas.hilger@icloud.com>
2023-09-06 08:28:33 +0200

    created

---
+++ src/main/java/de/lukasgp/extra_structures/ExtraStructures.java
@@ -0,0 +1,39 @@
+package de.lukasgp.extra_structures;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ExtraStructures.MOD_ID)
+public class ExtraStructures {
+    public static final String MOD_ID = "extra_structures";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ExtraStructures() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            
+        }
+    }
+}
